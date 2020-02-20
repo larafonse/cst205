4a5312e75694a7140138996df532b8716255dfd5
Nicolas Lara <nicolaslara@Nicolass-MacBook-Pro.local>
2020-02-19 17:52:53 -0800

    hw2 completed

---
+++ homework/hw2/median_filter.py
@@ -0,0 +1,31 @@
+# 
+# hw1_task1.py
+# Created by Nicolas Lara Fonseca and Rodrigo Andrade
+# 24 February 2020
+# 
+
+from PIL import Image
+from glob import glob
+from math import floor
+
+img_list=[]
+
+for filepath in glob('images/*.png'):
+	img_list.append(Image.open(filepath))
+
+new_img=Image.new('RGB',(img_list[0].width,img_list[0].height),'white')
+
+median = floor((len(img_list)+1)/2)
+print(median)
+
+
+for x in range(new_img.width):
+	for y in range(new_img.height):
+		pixel=[]
+		for im in img_list:
+			pixel.append(im.getpixel((x,y)))
+		pixel.sort()
+		new_img.putpixel((x,y),pixel[median])
+
+new_img.save('images/no_obj.png')
+new_img.show()
