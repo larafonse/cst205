c5b41eb50332eef769a1f72cf787561315fbe8a5
Nicolas Lara <nicolaslara@Nicolass-MBP.attlocal.net>
2020-03-01 15:10:55 -0800

    lab10 fully functional

--- labs/lab10/index.py
+++ labs/lab10/index.py
@@ -6,31 +6,49 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel,QHBoxLayout,QComboBox
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtGui import QIcon, QPixmap, QColor
 from PyQt5.QtCore import pyqtSlot
 my_list=['red','blue','green','yellow','black']
 dic = {
-'red':[(0,0,0),'#e234r2'],
-'blue':[(0,0,0),'#e234r2'],
-'green':[(0,0,0),'#e234r2'],
-'yellow':[(0,0,0),'#e234r2'],
-'black':[(0,0,0),'#e234r2']
+'red':[(255,0,0),'#FF0000'],
+'blue':[(0,0,255),'#0000FF'],
+'green':[(0,255,0),'#00FF00'],
+'yellow':[(255,255,0),'FFFF00'],
+'black':[(0,0,0),'#000000']
 }
+class colorWideget(QWidget):
+	def __init__(self):
+		super().__init__()
+		
+		
+	
+	def setColor(self,color):
+		self.setAutoFillBackground(True)
+		p = self.palette()
+		p.setColor(self.backgroundRole(), QColor(color[0],color[1],color[2]))
+		self.setPalette(p) 
 
 class MainWidget(QWidget):
 	"""docstring for MainWidget"""
 	def __init__(self):
 		super().__init__()
+		self.setAutoFillBackground(True)
+		p = self.palette()
+		p.setColor(self.backgroundRole(), QColor(0,0,0))
+		self.setPalette(p) 
 		
 
 		self.setWindowTitle('Colors')
-		self.label1 = QLabel('CST 205 Color Exchange', self)
-		self.label2 = QLabel('RGB:', self)
-		self.label3 = QLabel('HEX:', self)
+		self.label1 = QLabel('<font color="white">CST 205 Color Exchange</font>', self)
+		self.label2 = QLabel('<font color="white">RGB: </font>', self)
+		self.label3 = QLabel('<font color="white">HEX: </font>', self)
 
 		self.my_combo_box = QComboBox()
 		self.my_combo_box.addItems(my_list)
 		self.my_label = QLabel("")
+
+		self.btn = QPushButton("See Color!", self)
+		self.btn.clicked.connect(self.newWindow) 
 
 		vbox = QVBoxLayout()
 		vbox.addWidget(self.label1)
@@ -38,16 +56,23 @@ class MainWidget(QWidget):
 		vbox.addWidget(self.label2)
 		vbox.addWidget(self.label3)
 		vbox.addWidget(self.my_label)
+		vbox.addWidget(self.btn)
 
 		self.setLayout(vbox)
 		self.my_combo_box.currentIndexChanged.connect(self.update_ui)
+		self.second = colorWideget()
 
 	@pyqtSlot()
 	def update_ui(self):
-		my_text=self.my_combo_box.currentText()
 		my_index=self.my_combo_box.currentIndex()
-		self.label2.setText(f'RGB: {dic[my_list[my_index]][0]}')
-		self.label3.setText(f'HEX: {dic[my_list[my_index]][1]}')
+		self.label2.setText(f'<font color="white">RGB: {dic[my_list[my_index]][0]}</font>')
+		self.label3.setText(f'<font color="white">HEX: {dic[my_list[my_index]][1]}</font>')
+
+	def newWindow(self):
+		my_index=self.my_combo_box.currentIndex()
+		self.second.setColor(dic[my_list[my_index]][0])
+		self.second.show()
+
 
 app = QApplication(sys.argv)
 main = MainWidget()
