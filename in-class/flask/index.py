7635c13c1e390d3e72380eb0c2c1185f47ad8113
Nicolas Lara <nicolaslara@Nicolass-MacBook-Pro.local>
2020-03-04 17:15:06 -0800

    lab11 completed in class

--- in-class/flask/index.py
+++ in-class/flask/index.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app) 
 
+dict= {
+    'one':1,
+    'two':2,
+    'three':3}
 @app.route('/hello')
 def hello():
     return 'Hello World from Flask'
 
+# @app.route('/')
+# def root():
+#     return 'Hello You Are in the Root'
+
 @app.route('/')
-def root():
-    return 'Hello You Are in the Root'
+def home():
+    return render_template('template1.html', var = dict)
